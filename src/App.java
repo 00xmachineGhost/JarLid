26e5f29cd731182b7972d6ecedbbf763b5958c6b
Phas3Sm1th <torben.kast@gmail.com>
2022-12-01 15:33:53 +0100

    Init Code: Einmachglas Management

--- src/App.java
+++ src/App.java
@@ -2,5 +2,51 @@
 
 public class App {
 
+    //Attributes
+
+    String testVariable;
+    int ID;
+
+
+    //Main Method
+    public static void main(String[] args) {                            // Main-Funktion: wird grundsaetzlich als erstes ausgefuerht und ist auch das "Hauptprogramm"(Funktions Hoheit)
+        
+        App Jar = new App();
+
+        Jar.setContentLabel("Applesauce");
+        Jar.setContentID(25);
+
+        System.out.println(Jar.testVariable + "," + Jar.ID);
+
+    }
+
+
+    //Methods
     
+    public void setContentLabel(String nameLabel){
+
+        this.testVariable = nameLabel;
+
+    }
+
+    public void getContentLabel(String nameLabel) {
+        
+        nameLabel = this.testVariable;
+
+    }
+
+    public void setContentID(int labelID){
+
+        this.ID = labelID;
+
+    }
+
+    public void getContentID(int labelID) {
+        
+        labelID = this.ID;
+
+    }
+
+
+
 }
